67ac48b8aed67d112fa8e5dcc4c9d63386e2d5d1
vs4vijay <vs4vijay@gmail.com>
2023-02-26 00:44:50 +0530

    some webpack config changes

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: {
@@ -72,6 +73,7 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/styles.[chunkhash].css'
-    })
+    }),
+    new Dotenv()
   ]
 }
